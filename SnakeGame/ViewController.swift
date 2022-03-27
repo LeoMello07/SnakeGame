a3c27a4617cb5b68f2ba83215e6bbd96e69e3af8
Leonardo Mello <leonardomello2100@gmail.com>
2022-03-27 15:24:36 -0300

    Initial Commit

---
+++ SnakeGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnakeGame
+//
+//  Created by Leonardo Mello on 27/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
